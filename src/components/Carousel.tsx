815ccd7dc55a1ec61f877305a99a156bb9c30db4
new-user666 <110818393+new-user666@users.noreply.github.com>
2023-10-15 06:51:59 +0000

    some

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,12 +1,12 @@
-"use client"
-import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
+"use client";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
 
 interface Props {
-    images : any
+  images: any;
 }
 
-const Carousel = (props : Props) => {
+const Carousel = (props: Props) => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
@@ -19,14 +19,14 @@ const Carousel = (props : Props) => {
 
   return (
     <div className="relative w-full h-full">
-      {props.images.map((image : any , index : number) => (
+      {props.images.map((image: any, index: number) => (
         <div
           key={index}
           className={`absolute transition-opacity duration-500 w-full   ${
-            index === activeIndex ? 'opacity-100' : 'opacity-0'
+            index === activeIndex ? "opacity-100" : "opacity-0"
           }`}
         >
-          <img src={image} alt={`Image ${index}`} className="w-full h-full"   />
+          <img src={image} alt={`Image ${index}`} className="w-full h-full" />
         </div>
       ))}
     </div>
